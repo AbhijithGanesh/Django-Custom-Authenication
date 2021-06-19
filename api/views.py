043fbd5458b38fdc47d8b021290b0399d736b65c
Abhijith Ganesh <g.abhijtih14122003@gmail.com>
2021-06-19 17:28:48 +0530

    Initial Commit🚀

---
+++ api/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+class Register(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token = Token.objects.get_or_create(user=serializer.instance)
+        return Response(
+            {
+                "token": str(token[0]),
+                "username": serializer.instance.username,
+                "first_name": serializer.instance.first_name,
+                "last_name": serializer.instance.last_name,
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class Login(APIView):
+    def get(self, request):
+        return Response(    
+            {"Message":"Enter Details to Get a Response"},
+            status = status.HTTP_204_NO_CONTENT,
+        )
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(
+                {"error": "username or password not provided"},
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            )
+
+        user_obj = authenticate(username=username, password=password)
+
+        if not user_obj:
+            return Response(
+                {"error": "username or password is incorrect"},
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            )
+
+        token = Token.objects.get_or_create(user=user_obj)
+        return Response(
+            {
+                "detail": "Login Successfull",
+            },
+            status=status.HTTP_200_OK,
+        )
---
+++ djangoAuthenication/settings.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-p50hi#q=qredyg(4ur=@-(@2&hjlz7zah5y%y2u!e-_)mwk1jz'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'api.apps.ApiConfig'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'djangoAuthenication.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'djangoAuthenication.wsgi.application'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+PROJECT_NAME = 'Django-Custom-Authenication'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(
+    'static'
+)
